4f1ec14ce3fa48d07a757adde5d9ab3e64cfe313
Bintang Mada <44053984+bintangmada@users.noreply.github.com>
2026-07-05 20:06:08 +0700

    feat: enable JWT Bearer Authorization button in Swagger UI

--- backend/src/main/java/com/hrms/config/OpenApiConfig.java
+++ backend/src/main/java/com/hrms/config/OpenApiConfig.java
@@ -1,9 +1,12 @@
 package com.hrms.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import java.util.List;
@@ -38,7 +41,17 @@ public class OpenApiConfig {
                         .contact(new Contact()
                                 .name("Bintang Mada")
                                 .url("https://github.com/bintangmada")))
-                .servers(List.of(productionServer, localServer));
+                .servers(List.of(productionServer, localServer))
+                // Add Authorization option in Swagger UI
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
     }
 }
 
